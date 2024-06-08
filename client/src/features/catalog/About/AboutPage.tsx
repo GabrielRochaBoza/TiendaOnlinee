79426fb35588e1b1c7f906423efaee4cccd02dab
Gabriel Rocha <joynerrochab@gmail.com>
2024-06-07 19:08:13 -0600

    end of section 5

--- client/src/features/catalog/About/AboutPage.tsx
+++ client/src/features/catalog/About/AboutPage.tsx
@@ -1,10 +1,38 @@
-import { Typography } from "@mui/material";
+import { Alert, AlertTitle, Button, ButtonGroup, Container, List, ListItem, ListItemText, Typography } from "@mui/material";
+import agent from "../../../app/API/agent";
+import { useState } from "react";
 
 export default function AboutPage(){
+    const [validationErrors, setValidationErrors] = useState<string[]>([]);
+    function getValidationError(){
+        agent.TestErrors.getValidationError()
+            .then(()=> console.log("should not see this"))
+            .catch(error => setValidationErrors(error));
+    }
+
     return(
-    <Typography variant="h1">
-        About Page
-    </Typography>
+        <Container>
+            <Typography gutterBottom variant="h2"> ERRORS FOR TESTING PURPOSES</Typography>
+            <ButtonGroup fullWidth>
+                <Button variant="contained" onClick={() => agent.TestErrors.get400Error().catch(error => console.log(error))}>test 400 error</Button>
+                <Button variant="contained" onClick={() => agent.TestErrors.get401Error().catch(error => console.log(error))}>test 401 error</Button>
+                <Button variant="contained" onClick={() => agent.TestErrors.get404Error().catch(error => console.log(error))}>test 404 error</Button>
+                <Button variant="contained" onClick={() => agent.TestErrors.get500Error().catch(error => console.log(error))}>test 500 error</Button>
+                <Button variant="contained" onClick={getValidationError}>VALIDATION ERROR</Button>
+            </ButtonGroup>
+            {validationErrors.length > 0 && 
+                <Alert severity="error">
+                    <AlertTitle>Validation Errors</AlertTitle>
+                    <List>
+                        {validationErrors.map(error => (
+                            <ListItem key={error}>
+                                <ListItemText>{error}</ListItemText>
+                            </ListItem>
+                        ))}
+                    </List>
+                </Alert>
+            }
+        </Container>
     )
         
 }
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,3 +1,5 @@
+import agent from "../../app/API/agent";
+import LoadingComponent from "../../app/layout/loadingcomponent";
 import { Product } from "../../app/models/product";
 import ProductList from "./ProductList";
 import { useState, useEffect } from "react";
@@ -5,12 +7,15 @@ import { useState, useEffect } from "react";
 
 export default function Catalog (){
     const [products, setProducts] = useState<Product[]>([]);
+    const [Loading, setLoading] = useState(true)
 
     useEffect(() => {
-      fetch("http://localhost:5000/api/products")
-        .then(response => response.json())
-        .then(data => setProducts(data))
+      agent.Catalog.list().then(products =>setProducts(products))
+      .catch(error=>console.log(error))
+      .finally(()=>setLoading(false))
     },[])
+
+    if (Loading) return <LoadingComponent/>
   
     return(
         <>
